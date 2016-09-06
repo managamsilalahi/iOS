6b49a6aff54d9862175c980c292ac453541d2b8e
Managam Silalahi <managam.silalahi@gmail.com>
2016-09-06 21:21:49 +0700

    Stanford - Developing iOS 9 Apps with Swift - 1 Course Overview and iOS9 Introduction has finished.

--- Calculator/Calculator/ViewController.swift
+++ Calculator/Calculator/ViewController.swift
@@ -9,13 +9,30 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var display: UILabel!
+    
+    var userIsInTheMiddleOfTyping = false
     
     @IBAction func touchDigit(sender: UIButton) {
         let digit = sender.currentTitle!;
-        print("touched \(digit) digit");
+        if userIsInTheMiddleOfTyping {
+            let textCurrentlyInDisplay = display!.text!;
+            display!.text = textCurrentlyInDisplay + digit;
+        } else {
+            display!.text = digit
+        }
+        userIsInTheMiddleOfTyping = true
     }
-
+    
+    @IBAction func performOperation(sender: UIButton) {
+        userIsInTheMiddleOfTyping = false   
+        if let mathematicalSymbol = sender.currentTitle {
+            if mathematicalSymbol == "π" {
+                display.text = String(M_PI)
+            }
+        }
+        
+    }
 }
 
