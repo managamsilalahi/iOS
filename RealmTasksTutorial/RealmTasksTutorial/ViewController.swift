d2677957061c6f4ca4ec4585d035eb51ce91edd9
Managam Silalahi <managam.silalahi@gmail.com>
2017-01-07 12:03:00 +0700

    5. Use a Realm List to display Tasks in the table view

--- RealmTasksTutorial/RealmTasksTutorial/ViewController.swift
+++ RealmTasksTutorial/RealmTasksTutorial/ViewController.swift
@@ -7,12 +7,16 @@
 //
 
 import UIKit
+import RealmSwift
 
 class ViewController: UITableViewController {
+    
+    var items = List<Task>()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.setupUI()
+        self.setupFirstTask()
     }
     
     func setupUI() {
@@ -20,5 +24,26 @@ class ViewController: UITableViewController {
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
     }
     
+    func setupFirstTask() {
+        let firstTask = Task(value: ["text": "My First Task"])
+        items.append(firstTask)
+    }
+    
+}
+
+// MARK: - UITableViewDatSource
+
+extension ViewController {
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        let item = items[indexPath.row]
+        cell.textLabel?.text = item.text
+        cell.textLabel?.alpha = item.completed ? 0.5 : 1
+        return cell
+    }
 }
 
